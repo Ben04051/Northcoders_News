6102b65d5b35f71551ad826d91143879a88194ab
Ben04051 <benmitchell0405@hotmail.co.uk>
2024-01-15 14:17:19 +0000

    updated endpoints.json

--- controllers/articles.controllers.js
+++ controllers/articles.controllers.js
@@ -3,6 +3,7 @@ const {retrieveArticle} = require("../models/articles.models")
 exports.getArticle = (req, res, next) => {
     const {article_id} = req.params
     return retrieveArticle(article_id).then((article) => {
+        console.log(article)
         res.status(200).send({article})
     }).catch((err) => {
         next(err)
