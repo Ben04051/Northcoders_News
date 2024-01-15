5c74c211cdc1a2dfe682e7c78367f03d3c968765
Ben04051 <benmitchell0405@hotmail.co.uk>
2024-01-15 14:08:03 +0000

    complete task 4- get-api-articles-:articleid

---
+++ controllers/articles.controllers.js
@@ -0,0 +1,10 @@
+const {retrieveArticle} = require("../models/articles.models")
+
+exports.getArticle = (req, res, next) => {
+    const {article_id} = req.params
+    return retrieveArticle(article_id).then((article) => {
+        res.status(200).send({article})
+    }).catch((err) => {
+        next(err)
+    })
+}
