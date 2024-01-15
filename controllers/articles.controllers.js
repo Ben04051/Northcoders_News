03f45477e4a7201b22d5b0a74f9c266dfd300ee7
Ben04051 <benmitchell0405@hotmail.co.uk>
2024-01-15 14:18:11 +0000

    removed unwanted console log from articles.controllers

--- controllers/articles.controllers.js
+++ controllers/articles.controllers.js
@@ -3,7 +3,6 @@ const {retrieveArticle} = require("../models/articles.models")
 exports.getArticle = (req, res, next) => {
     const {article_id} = req.params
     return retrieveArticle(article_id).then((article) => {
-        console.log(article)
         res.status(200).send({article})
     }).catch((err) => {
         next(err)
